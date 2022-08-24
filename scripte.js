6fcb2222dbd5e0830bf65d90bebfa08ff7793600
Muhammad-Fasih2905 <const6545@gmail.com>
2022-08-24 15:00:57 +0500

    first commit

---
+++ scripte.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     if(this.scrollY > 20){
+//         $('.navbar').addClass("sticky");
+//     }else{
+//         $('.navbar').addClass("sticky");
+//     }
+// })
+// });
+alert("Welcome my Personal Website");
+console.log(alert);
